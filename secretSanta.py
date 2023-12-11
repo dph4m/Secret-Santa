9ad5275e51182d80b30f2c8138375d2c1b583683
Dylan Pham <46272950+dph4m@users.noreply.github.com>
2023-12-11 16:51:15 -0500

    secretSanta.py

--- secretSanta.py
+++ secretSanta.py
@@ -1,29 +1,42 @@
 import random
-import smtplib
-from email.message import EmailMessage
 
-#open text file and read lines
+
 def main():
+    #open text file and read lines
     tx = open("secretSanta.txt", 'r')
+    finishedMaps = ""
+    #opfile = open("opfile.txt", 'w')
     lns = tx.readlines()
     names = []
     mails = []
 
     #add all names in txt file to a name list, add all emails in txt file to an email list
     for ln in lns:
-        names.append(ln[0:ln.find(':')])
-        mails.append(ln[ln.find(':')+1: len(ln)])
-#for every sender, give a random receiver that doesn't already have a sender 
-    while len(names) > 0:
+        names.append(ln[0:ln.find(':')].rstrip())
+        mails.append(ln[ln.find(':')+1: len(ln)].rstrip())
+
+    mappedNames = []
+    mappedMails = []
+    i = len(names)
+    #for every sender, give a random receiver that doesn't already have a sender 
+    while i > 0:
         sender = random.randint(0,len(names)-1)
         receiver = random.randint(0,len(names)-1)
-        #random until sender isnt themself and the list isnt empty
-        while receiver == sender and len(names) > 1:
-            receiver = random.randint(0,len(names)-1)
-        print(f"{mails[sender]} sends gift to: {names[receiver]} \n")
-        #remove the sender and receiver mapped from the list
-        mails.pop(sender)
-        names.pop(receiver)
+        #check if the sender isnt mapped to self and sender doesnt already have a recipient and recipient isnt already mapped to a sender
+        if sender != receiver and sender not in mappedMails and receiver not in mappedNames:
+            finishedMaps = finishedMaps + (f" {len(names)-i+1}. {mails[sender]} sends gift to: {names[receiver]} \n")
+            #remove the sender and receiver mapped from the list
+            mappedNames.append(receiver)
+            mappedMails.append(sender)
+            i = i-1
+        #reset the algorithm until each gmail is mapped to someone thats not themself
+        else:
+            finishedMaps = ""
+            i = len(names)
+            mappedNames = []
+            mappedMails = []
+    print(finishedMaps)
+
 
 
 main()
